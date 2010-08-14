8c7f5854169cae9c6c613f6bc74fa03656ce52ed
Michael Jones <michael.daniel.jones@gmail.com>
2010-08-14 19:44:10 +0100

    second go at checking in the emacs files

---
+++ emacs.d/michaeljones.el
@@ -0,0 +1,6 @@
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet-0.6.1c")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+
