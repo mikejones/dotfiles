41573162323710f8c03e785d5af41af2d1000264
Michael Jones <michael.daniel.jones@gmail.com>
2010-08-15 16:24:28 +0100

    added all the elpa files created my own config file and plugins dir

--- emacs.d/michaeljones.el
+++ emacs.d/michaeljones.el
@@ -1,6 +1,22 @@
 (add-to-list 'load-path
-              "~/.emacs.d/plugins/yasnippet-0.6.1c")
+             "~/.emacs.d/plugins/yasnippet-0.6.1c")
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+
+
+;; use inconsolata in a nice small font
+(set-face-attribute 'default nil
+                    :family "inconsolata"
+                    :height 130)
+
+;; tabs are 2 spaces
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+;; fix so that command is meta key and alt is alt
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
 
